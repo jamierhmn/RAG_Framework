d39ec0468d3b9e5fd83b3f051317121a6907fe01
Ubuntu <ubuntu@ip-172-31-9-4.us-west-1.compute.internal>
2025-06-08 02:43:03 +0000

    zx

--- retrieval.py
+++
@@ -1,9 +0,0 @@
-
-def retrieve_documents(query_embedding, collection, top_k=2):
-    results = collection.query(query_embeddings=[query_embedding], n_results=top_k)
-
-    # Retrieve documents and metadata from the results
-    documents = results['documents']  # These should contain the actual content
-    metadata = results['metadatas']
-
-    return documents, metadata
